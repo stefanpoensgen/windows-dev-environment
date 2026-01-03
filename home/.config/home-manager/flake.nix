07ad7b0e610866db9957503af7deab1924d57848
Stefan Poensgen <mail@stefanpoensgen.de>
2026-01-03 11:09:22 +0100

    Refactor flake.nix to streamline inputs
    Removed unused inputs and added old nixpkgs version for compatibility.

--- home/.config/home-manager/flake.nix
+++ home/.config/home-manager/flake.nix
@@ -1,6 +1,5 @@
 {
   description = "Home Manager configuration of stefan";
-
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -8,37 +7,29 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-	devenv.url = "github:cachix/devenv/main";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-	jetbrains = {
-      url = "github:shyim/jetbrains-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    froshpkgs = {
-      url = "github:FriendsOfShopware/nur-packages";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Alte nixpkgs Version für PhpStorm
+    nixpkgs-old.url = "github:NixOS/nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb";
   };
-
-  outputs = { nixpkgs, home-manager, devenv, jetbrains, froshpkgs, ... }:
+  outputs = { nixpkgs, home-manager, nixpkgs-old, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-old = import nixpkgs-old {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
     in {
       homeConfigurations."stefan" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./home.nix ];
-		
-		extraSpecialArgs = {
-			jetbrains = jetbrains;
-			devenv = devenv;
-			froshpkgs = froshpkgs;
-		};
-
+        
+        extraSpecialArgs = {
+          inherit pkgs-old;
+        };
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
       };
